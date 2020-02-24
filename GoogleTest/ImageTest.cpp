79e62408098e0c9aa7e51cf5962ed7eff25fc5b3
Leonardo Biondi <leonardobiondi199@gmail.com>
2020-02-24 16:24:30 +0100

    addition test load and save

--- GoogleTest/ImageTest.cpp
+++ GoogleTest/ImageTest.cpp
@@ -15,6 +15,7 @@ TEST(Image, Test_Dimensions) {
 	Image img;
 	img.setWidth(20);
 	img.setHeight(20);
+
 	EXPECT_EQ(20, img.getWidth());
 	EXPECT_EQ(20, img.getHeight());
 }
@@ -22,6 +23,7 @@ TEST(Image, Test_Dimensions) {
 TEST(Image, Test_Channels) {
 	Image img;
 	img.setChannels(1);
+
 	EXPECT_EQ(1,img.getChannels());
 }
 
@@ -39,6 +41,23 @@ TEST(Image, Test_Compare) {
 	
 	EXPECT_TRUE(result);
 	
+}
+
+TEST(Image, Test_Load_AND_Save)
+{
+	Image img1;
+	string image1_Name("lena.ppm");
+	img1.Load(image1_Name);
+	img1.Save("lena_TestLoad.ppm");
+
+	Image img2;
+	string image2_Name("lena_TestLoad.ppm");
+	img2.Load(image2_Name);
+
+	//compare between two images
+	bool result = img1.compareImages(img1, img2);
+
+	EXPECT_TRUE(result);
 }
 
 
